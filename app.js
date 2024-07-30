d89f04d1fd2ee0885405501c83f9be60f5061a88
AnshAggarwalMAC <51926978+anshaggarwal28@users.noreply.github.com>
2024-07-30 04:29:16 -0400

    Update app.js

--- app.js
+++ app.js
@@ -12,9 +12,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public'))); 
 
-
-app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
